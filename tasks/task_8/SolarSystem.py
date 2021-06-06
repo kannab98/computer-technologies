d7e9e6b9be2585da3437cf3bfa04b94f50b73795
Tallahassee <aps98@mail.ru>
2021-06-06 19:47:19 +0300

    Update report, add imgs

--- tasks/task_8/SolarSystem.py
+++ tasks/task_8/SolarSystem.py
@@ -11,6 +11,8 @@ M0 = 1.9885*10**30
 
 class Sun():
     def __init__(self):
+        ''' Класс описывающий Солнце
+        '''
         self.mass = M0
         self.r = 0
         self.theta = 0
@@ -23,58 +25,86 @@ class Sun():
 
 class Comet():
     def __init__(self, name, mass, start_r, start_theta, v0_r, v0_theta):
-        self.name = name
-        self.mass = mass
-        self.start_r = start_r
-        self.start_theta = start_theta
+        ''' Класс для описания движения кометы в поле
+            тяготения тел Солнечной системы
+        '''
+        self.name = name # Название
+        self.mass = mass # Масса, кг
+        self.start_r = start_r  # Начальный радиус, м
+        self.start_theta = start_theta  # Начальный угол, радианы
         self.r = start_r
         self.theta = start_theta
-        self.v_r = v0_r
-        self.v_theta = v0_theta
-        self.x, self.y = self.convert_coord_polar_to_decart(start_r, start_theta)
-        self.v_x, self.v_y = self.convert_speed_polar_to_decart(v0_r, v0_theta, start_theta)
-        self.recorded_theta = [start_theta]
-        self.recorded_r = [start_r]
-    
-    def convert_coord_polar_to_decart(self, r, theta):
+        self.v_r = v0_r  # Радиальная скорость
+        self.v_theta = v0_theta # Угловая скорость
+        # Пересчет в декартовы координаты, т.к. вычисления в них проще
+        self.x, self.y = self.ccptd(start_r, start_theta)
+        self.v_x, self.v_y = self.csptd(v0_r, v0_theta, start_theta)
+        
+    def ccptd(self, r, theta):
+        ''' Конвертация координат из полярных в декартовы
+        '''
         x = r*np.cos(theta)
         y = r*np.sin(theta)
         return x, y
 
-    def convert_speed_polar_to_decart(self, v_r, v_theta, theta):
+    def csptd(self, v_r, v_theta, theta):
+        ''' Конвертация скоростей из полярных в декартовы
+        '''
         v_x = v_r*np.cos(theta) - v_theta*np.sin(theta)
         v_y = v_r*np.sin(theta) + v_theta*np.cos(theta)
         return v_x, v_y
     
-    def convert_coord_decart_to_polar(self, x, y):
+    def ccdtp(self, x, y):
+        ''' Конвертация координат из декартовых в полярные
+        '''
         r = np.sqrt(x**2 + y**2)
         theta = np.arctan2(y, x)
         return r, theta
+    
+    def csdtp(self, x, y, v_x, v_y):
+        ''' Конвертация скоростей из декартовых в полярные
+        '''
+        v_r = (x*v_x + y*v_y)/np.sqrt(x**2 + y**2)
+        v_theta = (x*v_y - y*v_x)/np.sqrt(x**2 + y**2)
+        return v_r, v_theta
 
     def calculate_distance_to_body(self, body):
+        ''' Расчет расстояния до тела
+        '''
         distance = np.sqrt( body.r**2 + self.r**2  - 2*body.r*self.r*np.cos(body.theta-self.theta))
         return distance
     
     def calculate_force_to_body(self, body):
+        ''' Расчет силы притяжения от одного тела
+        '''
         distance = self.calculate_distance_to_body(body)
         F = G*self.mass*body.mass/distance**2
-        body_x, body_y = self.convert_coord_polar_to_decart(body.r, body.theta)
+        body_x, body_y = self.ccptd(body.r, body.theta)
         F_x = F*(body_x - self.x)/distance
         F_y = F*(body_y - self.y)/distance
         return F_x, F_y
 
     def calculate_summary_acceleration(self, bodies):
+        ''' Расчет суммарного ускорения от нескольких тел
+        '''
         a_x_list, a_y_list = [], []
         for body in bodies:
             F_x, F_y = self.calculate_force_to_body(body)
             a_x_list.append(F_x/self.mass)
             a_y_list.append(F_y/self.mass)
-        a_x = np.mean(a_x_list)
-        a_y = np.mean(a_y_list)
+        a_x = np.sum(a_x_list)
+        a_y = np.sum(a_y_list)
         return a_x, a_y
 
     def model_func(self, t, data_vec):
-        #  data_vec = [x,y, vx,vy]
+        ''' Функция для решения путем интегрирования в solve_ivp.
+            На вход принимает data_vec вектор, имеющий следующие составляющие
+                data_vec = [x, y, vx, vy]
+            Возвращает правые части системы уравнений 
+                r' = v
+                v' = a = G sum(m_i/Ri^2)
+            в виде вектора [vx, vy, ax, ay]
+        '''
         coords = data_vec[0:2]
         vel = data_vec[2:]
 
@@ -82,15 +112,19 @@ class Comet():
         self.r = np.sqrt(coords[0]**2 + coords[1]**2)
         self.theta = np.arctan2(coords[1], coords[0])
 
+        # расчет положения тел в момент времени t
         for body in self.bodies:
             body.calculate_position_by_time(t)
         ax, ay = self.calculate_summary_acceleration(self.bodies)
         dvdt = [ax, ay]
-        # returns r', v'
         return np.hstack((vel, dvdt))
 
-    def evaluate_model(self, t_end, bodies, t_eval=None, method='RK45'):
+    def evaluate_model(self, t_end, bodies, t_eval=None, method='RK23'):
+        ''' Рассчет модели, с заданым временем окончания, методом интегрирования,
+            а также набором тел.
+        '''
         self.bodies = bodies
+        print(method)
         res = solve_ivp(self.model_func, t_end,
                         y0=[self.x, self.y, self.v_x, self.v_y],
                         t_eval=t_eval, method=method)
@@ -103,47 +137,54 @@ class CelestialBody():
         ''' Основной класс для описания орбит планет вокруг Солнца.
             Включает методы для рассчета движения во времени по заданной траектории.
         '''
-        self.name = name
-        self.mass = mass 
-        self.a = a  # Большая полуось
+        self.name = name # Название тела
+        self.mass = mass # Масса, кг
+        self.a = a       # Большая полуось, м
         self.ecc = eccentricity  # Эксцентриситет
-        self.angle_offset = np.pi*offset/180 # Долгота восходящего узла
-        self.start_theta = start_theta # Начальное положение
-        self.p = a*(1-self.ecc**2)
-        self.mu = G*M0
-        self.color = plot_color
+        self.angle_offset = np.pi*offset/180  # Долгота восходящего узла
+        self.start_theta = start_theta        # Начальное угловое положение
+        self.p = a*(1-self.ecc**2)  # Фокальный параметр
+        self.mu = G*M0              # Гравитационный параметр
+        self.color = plot_color     # Цвет графика
 
         self.const1 = self.a*(1-self.ecc**2)
         self.const2 = np.sqrt(self.mu*self.a*(1-self.ecc**2))
-        self.get_r(self.start_theta)
+        self.get_r(self.start_theta) # Рассчет расстояния по начальному углу theta
         self.theta = start_theta
 
+        # Запись координат - используется для отрисовки движения планет
         self.recorded_theta = [self.start_theta]
         self.recorded_r = [self.r]
     
     def calculate_position_by_time(self, t, points=100):
+        ''' Рассчет положения тела на орбите в определенный момент времени
+        '''
         dt = t/points
         for i in np.linspace(0, t, points):
             self.calculate_next_posistion(dt)
         r, theta = self.recorded_r[-1], self.recorded_theta[-1]
-        self.reset
+        self.reset()
         self.r, self.theta = r, theta
         return r, theta
 
     def get_r(self, theta):
-        ''' Get radius value for current angle theta.
+        ''' Рассчет радиуса положения тела в зависимости
+            от угла в полярных координатах
         '''
         self.r = self.const1/(1+self.ecc*np.cos(theta + self.angle_offset))
         return self.r
 
     def get_d_theta(self, dt):
-        '''  Get angle increment d_theta for current radius value
-             and time increment dt.
+        '''  Рассчет инкремента угла d_theta за определенный промежуток времени,
+             при текущем значении радиуса r
         '''
         d_theta = 1/self.r**2*self.const2*dt
         return d_theta
 
     def calculate_next_posistion(self, dt):
+        '''  Рассчет нового положения через dt секунд от текущего. 
+             Сохраняет значений угла и радиуса в список записанных значений.
+        '''
         d_theta = self.get_d_theta(dt)
         next_theta = self.recorded_theta[-1] + d_theta
         next_r = self.get_r(next_theta)
@@ -152,6 +193,9 @@ class CelestialBody():
         return next_r, next_theta
 
     def reset(self):
+        '''  Сброс записаных значений углов и радиусов в начальное положение.
+        '''
+        self.theta = self.start_theta
         self.get_r(self.start_theta)
         self.recorded_theta = [self.start_theta]
         self.recorded_r = [self.r]
--- tasks/task_8/simulation.py
+++ tasks/task_8/simulation.py
@@ -2,35 +2,48 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from SolarSystem import System, Planets, Comet
-fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
+from configs import configs
 
-# Начальные параметры кометы
-TheComet = Comet("Comet",
-                 1*10**11,   # Масса
-                 50*10**11,  # Начальный радиус
-                 3*np.pi/4,  # Начальный угол
-                 0,          # Начальная радиальная скорость
-                 1500        # Начальная угловая скорость
-                 )
+use_config = "RunBy"
+if use_config:
+    cfg = configs[use_config]
+    TheComet = Comet("Comet", cfg[0], cfg[1], cfg[2], cfg[3], cfg[4])
+else:
+    TheComet = Comet("Comet",
+                     8*10**22,   # Масса, кг
+                     1.5*10**12,  # Начальный радиус, м
+                     0,  # Начальный угол, радианы
+                     0,          # Начальная радиальная скорость, м/c
+                     10000        # Начальная угловая скорость, м/с
+                     )
 
-# Параметры симуляции
-integration_method = 'RK23'
-# Время моделирования
-t_end = 1*10**9      
-# Шаг моделирования   
-dt = 2*10**6
+
+### Параметры симуляции ###
+
+# Метод интегрирования в функцие np.solve_ivp()
+integration_method = 'Radau'
+# Время моделирования в секундах
+t_end = 40*10**8
+# t_end = 3.154*10**7  # Земной год
+# Шаг времени в секундах  
+dt = 2*10**4
+# Временные отсчеты в которых посчитать значение координат
 t = np.arange(0, t_end, dt)
 
-# Моделируем модель кометы в Солнечной системе
+# Моделируем движение кометы в Солнечной системе
+# res = TheComet.evaluate_model([0, t_end], [System[0]], t_eval=t, method=integration_method)
 res = TheComet.evaluate_model([0, t_end], System, t_eval=t, method=integration_method)
-time = res.t
-coords = res.y[0:2]
-vels = res.y[2:]
+res_time = res.t
+comet_coords = res.y[0:2]
+comet_vels = res.y[2:]
 # Конвертируем координаты в полярные для дальнейшего построения графиков
-comet_r, comet_theta = TheComet.convert_coord_decart_to_polar(
-    coords[0], coords[1])
+comet_r, comet_theta = TheComet.ccdtp(comet_coords[0], comet_coords[1])
+comet_vr, comet_vtheta = TheComet.csdtp(comet_coords[0], comet_coords[1], comet_vels[0], comet_vels[1])
 
-### Далее код отвечает за отрисовку
+### Далее код отвечает за отрисовку ###
+fig, ax = plt.subplots(figsize=(8, 6), dpi=100, subplot_kw={'projection': 'polar'})
+fig2, ax2 = plt.subplots(figsize=(8, 6), dpi=100)
+fig3, ax3 = plt.subplots(figsize=(8, 6), dpi=100)
 
 # Ресетим планеты в их изначальное положение
 for Planet in Planets:
@@ -43,13 +56,36 @@ for i, val in enumerate(t):
 
 # Строим траектории планет и кометы в полярных координатах
 for Planet in Planets:
-    ax.plot(Planet.recorded_theta, Planet.recorded_r,
-            '--', color=Planet.color, label=Planet.name)
-    ax.plot(Planet.recorded_theta[-1],
-            Planet.recorded_r[-1], 'o', color=Planet.color)
+    ax.plot(Planet.recorded_theta, Planet.recorded_r, '--', color=Planet.color, label=Planet.name)
+    ax.plot(Planet.recorded_theta[-1], Planet.recorded_r[-1], 'o', color=Planet.color)
+
+    ax2.plot(t, Planet.recorded_r[0:-1], label=Planet.name, color=Planet.color)
+
 ax.plot(comet_theta, comet_r, color='k', label="Comet")
 ax.plot(comet_theta[-1], comet_r[-1], 'o', color='k')
 ax.plot(0, 0, 'o', color='#FFDF00', label="Sun")
+ax.legend()
 
-plt.legend()
+ax2.plot(res_time, comet_r, color='k', label="Comet")
+ax2.grid(which='both')
+ax2.legend(loc=3)
+ax2.set_yscale('log')
+ax2.set_xlabel("Время, с")
+ax2.set_ylabel("Радиус траектории, м")
+
+ax3.plot(res_time, comet_vr, label='$V_r$')
+ax3.plot(res_time, comet_vtheta, label='$V_{\\theta}$')
+ax3.set_xlabel("Время, с")
+ax3.set_ylabel("Скорость, м/c")
+ax3.grid(which='both')
+ax3.legend(loc=3)
+
+if use_config:
+    fig.savefig('imgs_8/trj{}.png'.format(use_config), bbox_inches='tight')
+    fig2.savefig('imgs_8/r{}.png'.format(use_config), bbox_inches='tight')
+    fig3.savefig('imgs_8/v{}.png'.format(use_config), bbox_inches='tight')
+else:
+    fig.savefig('imgs_8/trj_custom.png', bbox_inches='tight')
+    fig2.savefig('imgs_8/r_custom.png', bbox_inches='tight')
+    fig3.savefig('imgs_8/v_custom.png', bbox_inches='tight')
 plt.show()
